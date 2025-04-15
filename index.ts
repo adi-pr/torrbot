4e5ac271612b6397417e6432bfdbb3e443af5043
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2025-04-15 15:05:59 -0400

    Added whatsapp-web-js

--- index.ts
+++ index.ts
@@ -1,6 +1,26 @@
-import { transmission } from "./services/transmission";
+import { Client, LocalAuth } from "whatsapp-web.js";
+// import * as qrcode from "qrcode-terminal";
 
-async function main() {
-  const res = await transmission.getAllData();
-  console.log(res);
-}
+export const client = new Client({
+    authStrategy: new LocalAuth({
+        dataPath: '.wwebjs_cache'
+    }),
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+})
+
+// client.on('qr', (qr) => {
+//     qrcode.generate(qr, { small: true })
+// })
+
+client.on('ready', () => {
+    const contactIDs = ['5926002376@c.us', "5926576607@c.us"];
+    
+    contactIDs.forEach((contactId) => {
+        client.sendMessage(contactId, "Bot is ready!");
+    })
+});
+
+client.initialize();
