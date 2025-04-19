eec9c2cc698b45f4e2bc8fee506b63b093ef64e9
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2025-04-19 12:59:28 -0400

    Enhances bot functionality and security
    Refactors admin contact handling for reuse and clarity.
    Adds message handling to process commands and notify unauthorized access attempts.
    Logs authentication failures and disconnection events for debugging and monitoring.

--- index.ts
+++ index.ts
@@ -8,30 +8,53 @@ export const client = new Client({
     }),
 });
 
-export function sendMessage(content: string) {
-    const contactIDs = process.env.ADMIN_NUMBERS?.split(',') || [];
+const adminContactIDs = process.env.ADMIN_NUMBERS?.split(',') || [];
 
-    contactIDs.forEach((contactId) => {
+export function sendMessage(content: string) {
+    adminContactIDs.forEach((contactId) => {
         client.sendMessage(contactId, content);
     })
 }
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('Client is ready!');
+
+    const adminContactIDs = process.env.ADMIN_NUMBERS?.split(',') || [];
+
+    await checkTorrentStatus()
+
+    adminContactIDs.forEach((contactId) => {
+        sendMessage("Bot is ready!");
+    })
 })
 
+// Uncomment the following lines to be able to log into whatsapp and generate the .wwedjs_cache folder
 // client.on('qr', (qr) => {
 //     qrcode.generate(qr, { small: true })
 // })
 
-client.on('ready', async () => {
-    const contactIDs = process.env.ADMIN_NUMBERS?.split(',') || [];
+client.on('message', async (message) => {
+    const content = message.body.toLowerCase();
 
-    await checkTorrentStatus()
+    if (adminContactIDs.includes(message.from)) {
+        if (content.startsWith('!')) {
+            message.reply('Command received: ' + content);
+        }
+    } else {
+        message.reply('Unrecognized user. Please contact the admin.');
 
-    contactIDs.forEach((contactId) => {
-        client.sendMessage(contactId, "Bot is ready!");
-    })
+        // Notifdy admin about unauthorized access attempt
+        const adminMessage = `Unauthorized access attempt detected from ${message.from} (${message.deviceType}) at ${message.timestamp}.\n\nMessage: ${message.body}`;
+        sendMessage(adminMessage);
+    }
+});
+
+client.on('disconnected', (reason) => {
+    console.log('Client was logged out', reason);
+});
+
+client.on('auth_failure', () => {
+    console.error('Authentication failed');
 });
 
 client.initialize();
